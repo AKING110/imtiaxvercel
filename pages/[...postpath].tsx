34b6af8b18b52b5298632efac3e5983f2eb147ba
IMTIAZ AKING <98144321+AKING110@users.noreply.github.com>
2024-08-01 10:05:15 +0500

    Update [...postpath].tsx

--- pages/[...postpath].tsx
+++ pages/[...postpath].tsx
@@ -19,7 +19,7 @@ export const getServerSideProps: GetServerSideProps = async (ctx) => {
 			redirect: {
 				permanent: false,
 				destination: `${
-					`https://saveourstateok.org/` + encodeURI(path as string)
+					`https://noverima.com/` + encodeURI(path as string)
 				}`,
 			},
 		};
